31488ae631c2017795fbaff08a0e415509f03793
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-12 11:16:21 +0100

    Tests for function parse-boolean

--- test/clj_utils/utils_test.clj
+++ test/clj_utils/utils_test.clj
@@ -361,4 +361,15 @@
         (is (thrown? NullPointerException (replaceAll "a" "a" nil)))
         (is (thrown? NullPointerException (replaceAll nil nil "b")))))
 
+(deftest test-parse-boolean
+    "Check the behaviour of function emender-jenkins.config/parse-boolean."
+    (are [x y] (= x y)
+        true (parse-boolean "true")
+        true (parse-boolean "True")
+        false (parse-boolean "false")
+        false (parse-boolean "False")
+        false (parse-boolean "")
+        false (parse-boolean "unknown")
+        false (parse-boolean nil)))
+
 
