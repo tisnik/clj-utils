9157ac067b526adc5dc7f826f3da63ad57d701dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-04 09:18:59 +0200

    New utility function

--- src/clj_utils/utils.clj
+++ src/clj_utils/utils.clj
@@ -10,18 +10,22 @@
 ;      Pavel Tisnovsky
 ;
 
+
 (ns clj-utils.utils
     "Various utility functions used by other modules.")
+
 
 (defn third
     "Simple utility function - returns third item from a given sequence, nil if third item does not exist."
     [coll]
     (nth coll 2 nil))
 
+
 (defn fourth
     "Simple utility function - returns fourth item from a given sequence, nil if third item does not exist."
     [coll]
     (nth coll 3 nil))
+
 
 (defn substring
     "Call method String.substring()."
@@ -30,52 +34,63 @@
     ([^String s from]
      (.substring s from)))
 
+
 (defn startsWith
     "Call method String.startsWith()."
     [^String s pattern]
     (.startsWith s pattern))
 
+
 (defmacro starts-with
     [s pattern]
     `(startsWith ~s ~pattern))
+
 
 (defn endsWith
     "Call method String.endsWith()."
     [^String s pattern]
     (.endsWith s pattern))
 
+
 (defmacro ends-with
     [s pattern]
     `(endsWith ~s ~pattern))
+
 
 (defn contains
     "Call method String.contains()."
     [^String s pattern]
     (.contains s pattern))
 
+
 (defn replaceAll
     "Call method String.replaceAll()."
     [^String s pattern replacement]
     (.replaceAll s pattern replacement))
 
+
 (defmacro replace-all
     [s pattern replacement]
     `(replaceAll ~s ~pattern ~replacement))
+
 
 (defn get-exception-message
     "Retrieve a message from given exception."
     [^java.lang.Exception exception]
     (.getMessage exception))
 
+
 (defn parse-int
     "Parse the given string as an integer number."
     [^String string]
     (java.lang.Integer/parseInt string))
 
+
 (defn parse-float
     "Parse the given string as a float number."
     [^String string]
     (java.lang.Float/parseFloat string))
+
 
 (defn parse-boolean
     "Parse the given string as a boolean value."
@@ -84,6 +99,7 @@
         (= string "True")
         (= string "yes")
         (= string "Yes")))
+
 
 (defn parse-color
     "Parse the value in format #rrggbb and return red, green, blue triple; nil instead."
@@ -97,6 +113,7 @@
                (Integer/parseInt green-part 16)
                (Integer/parseInt blue-part  16)])))
 
+
 (defn parse-color->Color
     "Conversion from color triple (vector) into an instance java.awt.Color."
     [^String string]
@@ -105,6 +122,13 @@
             (new java.awt.Color (first color-triple)
                                 (second color-triple)
                                 (third color-triple)))))
+
+
+(defn rgb->Color
+    "Convert a RGB vector into color"
+    [rgb]
+        (apply #(java.awt.Color. %1 %2 %3) rgb))
+
 
 (defn throw-exception
     ( [message]
