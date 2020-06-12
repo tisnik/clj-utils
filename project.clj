e16cdde23251f1c60d4a1c008149b2fac05c618e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-12 07:55:15 +0200

    Reformatted by zprint

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2017, 2018  Pavel Tisnovsky
+;  (C) Copyright 2017, 2018, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -11,13 +11,13 @@
 ;
 
 (defproject org.clojars.tisnik/clj-utils "0.3.0-SNAPSHOT"
-    :description "Simple library to read and parse INI files."
-    :url "https://github.com/tisnik/clj-utils"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]]
-    :plugins [[test2junit "1.1.0"]
-              [codox "0.8.11"]
-              [lein-cloverage "1.0.6"]]
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "Simple library to read and parse INI files."
+  :url "https://github.com/tisnik/clj-utils"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[test2junit "1.1.0"]
+            [codox "0.8.11"]
+            [lein-cloverage "1.0.6"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
