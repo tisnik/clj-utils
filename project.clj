2294613e312ba7801eb435256d4889785d82a7ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-12 10:54:27 +0100

    Bump-up Clojure version

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
   :url "https://github.com/tisnik/clj-utils"
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[codox "0.10.7"]
             [test2junit "1.1.0"]
             [lein-cloverage "1.0.6"]]
