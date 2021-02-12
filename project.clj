b0523400aa1d1a2742a2ceb2413b3a382040dc08
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-12 10:54:03 +0100

    Bump-up Codox version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2017, 2018, 2020  Pavel Tisnovsky
+;  (C) Copyright 2017, 2018, 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -16,8 +16,8 @@
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[test2junit "1.1.0"]
-            [codox "0.8.11"]
+  :plugins [[codox "0.10.7"]
+            [test2junit "1.1.0"]
             [lein-cloverage "1.0.6"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
